e7f5ba67c9c685c0b47872b51b7afd1a8210aa2a
bigdatacon <link17892020@gmail.com>
2024-05-17 16:25:09 +0300

    Добавлена возможность сборки с++ без питон и cmake

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "utils.h"
+#include "DataContainer.h"
 
 int main() {
+    std::cout << "Пример Использование add_arrays:  " << std::endl;
     std::vector<int> a = {1, 2, 3};
     std::vector<int> b = {434234, 5, 6};
     std::vector<int> result = add_arrays(a, b);
@@ -9,5 +11,17 @@ int main() {
         std::cout << num << " ";
     }
     std::cout << std::endl;
+
+
+///Использование класса DataContainer
+    std::cout << "Пример Использование класса DataContainer:  " << std::endl;
+    auto container = DataContainer();
+    container.add(10);
+    container.add(20);
+    std::cout << "container.get_data()" << std:: endl;
+    for (auto el :container.getData()){
+        std::cout << el << std:: endl;  // Выведет [10, 20]
+    }
+
     return 0;
 }
